97ee507c08ba8b991d3873164e4d1fa548cb4c47
mcalex468 <alexmaco468@gmail.com>
2024-01-10 09:38:15 +0100

    Primer Commit

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    kotlin("jvm") version "1.9.21"
+    application
+    id("org.jetbrains.dokka") version "1.9.21"
+
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    // Unit Testing
+    testImplementation(kotlin("test"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
+
+
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(17)
+}
---
+++ src/main/kotlin/Funcions.kt
@@ -0,0 +1,32 @@
+fun cuotaFixa () {
+    val fix = 6.0
+
+
+
+}
+
+fun consumAigua () {
+    var cuotaVariable = 0.0
+    val consum = readInt()
+    when (consum){
+        consum in 0..49 = cuotaVariable = 0
+        consum in 50..200 = cuotaVariable = 0.15
+        consum > 200 = cuotaVariable = 0.30
+
+    }
+
+
+}
+
+fun familiaNombrosa () {
+    val nombrosaMonoparental = readBoolean()
+    println("Quants membres sou ?")
+    val membres = readInt()
+    val descompte = 0.0
+    if (nombrosaMonoparental) {
+        10*
+    }
+
+
+
+}
